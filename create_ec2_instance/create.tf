4f77490e630969f6525e250100b37d954eca228d
Barnabas Vizy Admin <bvizyadmin@psi-it.com>
2019-06-24 14:16:07 -0400

    setup remote backend

--- create_ec2_instance/create.tf
+++ create_ec2_instance/create.tf
@@ -1,9 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "vizzyy-terraform"
+    key    = "create/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   profile    = "default"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
